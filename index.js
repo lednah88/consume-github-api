5af2890e55970c28de09e8b0384506626cb048c2
lednah88 <lednah97@gmail.com>
2020-07-02 15:01:05 +0200

    request fixed

--- index.js
+++ index.js
@@ -1,18 +1,18 @@
 const fetch = require('./node_modules/node-fetch');
 
 function getRepos(repoName, startDate, endDate) {
-	var startingDate = new Date(startDate);
-	var endingDate = new Date(endDate);
+	let startingDate = new Date(startDate);
+	let endingDate = new Date(endDate);
           fetch(`https://api.github.com/repos/github-tools/${repoName}/pulls`)
           .then((response) => response.json())
 		  .then((data) => {
-			  j = 0
+			 let j = 0
 			  for(datas in data){
 				  if(new Date(data[j].created_at) >= startingDate  && new Date(data[j].created_at) <= endingDate && new Date(data[j].updated_at) >= startingDate  && new Date(data[j].updated_at) <= endingDate){
-                      console.log(`pull reguest created at ${data[j].created_at}`);
-                      console.log(`pull reguest updated at ${data[j].updated_at}`);
-                      console.log(`pull reguest merged at ${data[j].merged_at}`);
-                      console.log(`pull reguest closed at ${data[j].closed_at}`);
+                      console.log(`pull request created at ${data[j].created_at}`);
+                      console.log(`pull request updated at ${data[j].updated_at}`);
+                      console.log(`pull request merged at ${data[j].merged_at}`);
+                      console.log(`pull request closed at ${data[j].closed_at}`);
 				  }
 				  j++;
 				}
